ad7f2eb3af891b7860be3fe925ae6bfe34515733
kobelev anton <kobeleva166@gmail.com>
2025-05-29 20:26:47 +0500

    Создан блок с преимуществами

--- frontend/components/MainPage/Benefits.vue
+++ frontend/components/MainPage/Benefits.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        
+    <div class="container pt-[50px] h-[600px] flex flex-col items-center">
+        <h4 class="text-[32px] font-semibold text-[#150981] text-center mb-[60px]">Почему выбирают нас?</h4>
+        <div class="h-[450px] w-[100%] flex items-center justify-between">
+            <div class="group flex flex-col items-center w-[200px] hover:h-[300px] hover:bg-[#150981] duration-400 rounded-[20px] pt-[20px]">
+                <svg class="group-hover:stroke-white stroke-[#150981] mb-[35px]" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/  svg">
+                    <path d="M2.98849 55.1568L18.892 63.1085L26.8438 79.0121L34.7955 71.0603V59.1326L48.7111 45.2171L60.6388 81L72.5664 69.0724L62.6267 31.3015L78.5302 15.3979C81.824 12.1042 81.824 6.76403 78.5302 3.4703C75.2365 0.176567 69.8963 0.176568 66.6026 3.4703L50.6991 19.3738L12.9282 9.43412L1.00055 21.3618L36.7835 33.2894L22.8679 47.205H10.9403L2.98849 55.1568Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <p class="text-[22px] font-semibold text-center hidden group-hover:inline text-white mb-[30px]">Быстро</p>
+                <p class="text-lg text-center hidden group-hover:inline text-white">Расчет за 2 минуты</p>
+            </div>
+
+
+            <div class="group flex flex-col items-center w-[200px] hover:h-[300px] hover:bg-[#150981] duration-400 rounded-[20px] pt-[20px]">
+                <svg class="group-hover:stroke-white stroke-[#150981] mb-[35px]" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M60 62.5V80C60 82.7614 57.7614 85 55 85H45C42.2386 85 40 82.7614 40 80V62.5M75 40C75 53.8071 63.8071 65 50 65C36.1929 65 25 53.8071 25 40C25 26.1929 36.1929 15 50 15C63.8071 15 75 26.1929 75 40Z" stroke-width="2"/>
+                </svg>
+
+                <p class="text-[22px] font-semibold text-center hidden group-hover:inline text-white mb-[30px]">Профессионально</p>
+                <p class="text-lg text-center hidden group-hover:inline text-white">10+ лет опыта</p>
+            </div>
+
+
+            <div class="group flex flex-col items-center w-[200px] hover:h-[300px] hover:bg-[#150981] duration-400 rounded-[20px] pt-[20px]">
+                <svg class="group-hover:stroke-white stroke-[#150981] mb-[35px]" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M27.5 36.6667V32.8571C27.5 20.195 37.5357 10 50 10C62.4643 10 72.5 20.195 72.5 32.8571V36.6667M27.5 36.6667C23.375 36.6667 20 40.0952 20 44.2857V82.3809C20 86.5714 23.375 90 27.5 90H72.5C76.625 90 80 86.5714 80 82.3809V44.2857C80 40.0952 76.625 36.6667 72.5 36.6667M27.5 36.6667H72.5M50 67.5V57.5" stroke-width="2" stroke-linecap="round"/>
+                </svg>
+
+                <p class="text-[22px] font-semibold text-center hidden group-hover:inline text-white mb-[30px]">Без скрытых платежей</p>
+                <p class="text-lg text-center hidden group-hover:inline text-white">Фиксированная цена</p>
+            </div>
+        </div>
+        <div class="w-[50px] h-[3px] bg-[#150981]"></div>
     </div>
 </template>
+
+<!-- stroke="#150981" -->
--- frontend/components/TheHeader.vue
+++ frontend/components/TheHeader.vue
@@ -4,7 +4,7 @@
             <img src="../assets/image/logo.png" alt="logo" class="size-[60px]">
             <div class="flex gap-[20px]">
                 <!-- <div class=""> -->
-                <NuxtLink to="/reg" class="block text-[#150981] w-[100px] h-[30px] bg-[white] flex justify-center items-center">Sign up</NuxtLink>
+                <NuxtLink to="/reg" class="text-[#150981] w-[100px] h-[30px] bg-[white] flex justify-center items-center">Sign up</NuxtLink>
                 <!-- </div> -->
                <NuxtLink to="/enter" class="text-[white]">Sign in</NuxtLink>
             </div>
